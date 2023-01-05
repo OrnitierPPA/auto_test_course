ff3cef2b7d1596ef8cd20e9e2d4ba05ef7961c00
Arkady Popov <associatedtolife@mail.ru>
2023-01-05 11:37:42 +0400

    Добавил поддержку двух браузеров

--- Lesson_3/conftest.py
+++ Lesson_3/conftest.py
@@ -2,22 +2,25 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-# @pytest.fixture(scope="function")
-# def browser():
-#     print("\nstart browser for test..")
-#     browser = webdriver.Firefox()
-#     browser.maximize_window()
-#     yield browser
-#     print("\nquit browser..")
-#     browser.quit()
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='firefox',
+                     help="Choose browser: chrome or firefox")
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    options = Options()
-    options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-    browser = webdriver.Firefox(options=options)
-    browser.maximize_window()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        options = Options()
+        options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+        browser = webdriver.Firefox(options=options)
+        browser.maximize_window()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
